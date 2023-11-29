10f9b9f18e1b3f3609a6d805bc571175da6819a6
FuadHussen <142115071+FuadHussen@users.noreply.github.com>
2023-11-29 11:15:04 +0100

    showed character

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -6,8 +6,16 @@ class World {
         new Chicken()
     ];
 
+    ctx;
+
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
+
 
     draw() {
-
+        this.ctx.drawImage(this.character.img,this.character.y, this.character.x, this.character.width,  this.character.height);
     }
 }
