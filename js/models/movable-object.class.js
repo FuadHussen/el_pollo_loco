fa156d559fd9f954dc8b58614e45a1e29f5e94d6
FuadHussen <142115071+FuadHussen@users.noreply.github.com>
2023-12-21 12:20:07 +0100

    animate big chicken and character

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -4,11 +4,25 @@ class MovableObject {
     height = 150;
     width = 100;
     img;
+    imageCache = {};
+    currentImage = 0;
 
 
     loadImage(path) {
         this.img = new Image(); //this.img = document.getElementById('img') <img id="img">
         this.img.src = path;
+    }
+
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image1.png', ...]
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        })
     }
 
 
