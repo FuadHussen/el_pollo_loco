073735f5b725f360f9be0bdff46f813eb8490f38
FuadHussen <142115071+FuadHussen@users.noreply.github.com>
2024-01-08 17:28:12 +0100

    added endboss

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -28,6 +28,14 @@ class MovableObject {
     }
 
 
+    playAnimation(images){
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }
+
+
     moveLeft() {
         setInterval(() => {
             this.x -= this.speed;
